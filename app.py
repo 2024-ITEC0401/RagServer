05c80f69b3191bee3753e18031ddab217bec7203
LeeDongjae <leedongjae625@gmail.com>
2024-12-01 01:03:33 +0900

    hotfix: ip 추가

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def spec():
 def hello_world():
     return "Hello World"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 #gcloud config get-value project
